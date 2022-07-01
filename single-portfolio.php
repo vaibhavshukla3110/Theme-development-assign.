980f781e0efe90948eb16ce110bcdc3c9ecdbb7b
Vaibhav Shukla <vaibhav.shukla@hbwsl.com>
2022-07-01 09:21:36 +0530

    create a single portfolio post page

---
+++ single-portfolio.php
@@ -0,0 +1,30 @@
+<?php
+get_header();
+
+if(have_posts()){
+    while(have_posts()){
+        the_post();
+    }
+}
+
+get_template_part( 'content', 'features' );
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="blog-column">
+            <h2 class="single-blog-heading"><?php the_title(); ?></h2>
+            <div class="single-blog-details">
+                <span class="single-blog-post-details">by <?php the_author(); ?></a> on <?php the_date(); ?></span>
+                <span class="single-blog-post-details single-blog-post-comments"><?php comments_number(); ?></span>
+            </div>
+
+            <?php get_template_part('content','article'); ?>
+
+            <?php comments_template() ?>
+        </div>
+        <?php get_template_part('content','sidebar'); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
